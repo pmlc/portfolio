5715f0a2880b4a55a5ce52590d773604e9f098f3
Pierre Le Count <pmlc@iinet.net.au>
2018-09-29 06:04:38 +0000

    integrated new andcreate actions for Portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,35 @@
 class PortfoliosController < ApplicationController
+  # before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
    
   def index 
     @portfolio_items = Portfolio.all 
   end 
- 
+  
+  def new
+    @portfolio_item = Portfolio.new
+  end
+  
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+    end
+
 end
