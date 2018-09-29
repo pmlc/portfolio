8e29373c2a150621c93e1bf37a9807de2a550ca9
Pierre Le Count <pmlc@iinet.net.au>
2018-09-29 03:49:31 +0000

    added skills

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180928060357) do
+ActiveRecord::Schema.define(version: 20180929032300) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "percent_utilized"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
