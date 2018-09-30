87ce594e0ccc491a96dd58c8e53a6fcbc72cb360
Pierre Le Count <pmlc@iinet.net.au>
2018-09-30 04:20:00 +0000

    Implemented destory acrtion;

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,6 +41,19 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id]) 
   end 
 
+  def destroy 
+    # Perform the lookup 
+    @portfolio_item = Portfolio.find(params[:id]) 
+  
+    # Destroy/delete the record 
+    @portfolio_item.destroy 
+  
+    # Redirect 
+    respond_to do |format| 
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' } 
+    end 
+  end 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
