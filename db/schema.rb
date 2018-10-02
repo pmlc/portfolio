271fb0d44fa5532a4050fcf634423d91ce1f6f75
Pierre Le Count <pmlc@iinet.net.au>
2018-10-02 23:57:08 +0000

    implemented concern for managing image data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002221950) do
+ActiveRecord::Schema.define(version: 20181002234038) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
