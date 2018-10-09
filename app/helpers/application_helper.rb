1b41821127b807296381719d09fc569e48247280
Pierre Le Count <pmlc@iinet.net.au>
2018-10-09 22:50:00 +0000

    Updated master layout styles and helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser) 
-      (link_to "Register", new_user_registration_path) + 
-      "<br>".html_safe + 
-      (link_to "Login", new_user_session_path) 
+      (link_to "Register", new_user_registration_path, class: style) + 
+      " ".html_safe + 
+      (link_to "Login", new_user_session_path, class: style)  
     else 
-      link_to "Logout", destroy_user_session_path, method: :delete 
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style 
     end 
   end
   
@@ -16,5 +16,11 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting") 
     end
   end 
+  
+  def copyright_generator
+    ##  DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    "Copyright - Pierre Le Count, 'All rights reserved"
+  end 
+
 
 end
