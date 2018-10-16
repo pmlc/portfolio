4a1144e435b66add14aa291b3aa7578e6ae92fc1
Pierre Le Count <pmlc@iinet.net.au>
2018-10-16 21:18:44 +0000

    Implemented persitant position updates for portfolio drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,67 +6,75 @@ class PortfoliosController < ApplicationController
   def index 
     @portfolio_items = Portfolio.by_position
   end 
-
-    def angular 
-      @angular_portfolio_items = Portfolio.angular 
-    end 
-
-    def new 
-      @portfolio_item = Portfolio.new 
-      3.times { @portfolio_item.technologies.build } 
-    end 
-
-    def create 
-      @portfolio_item = Portfolio.new(portfolio_params) 
   
-      respond_to do |format| 
-        if @portfolio_item.save 
-          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' } 
-        else 
-          format.html { render :new } 
-        end 
-      end 
-    end 
-
-    def edit 
+  def sort 
+    params[:order].each do |key, value| 
+      Portfolio.find(value[:id]).update(position: value[:position]) 
     end 
   
-    def update 
-      respond_to do |format| 
-        if @portfolio_item.update(portfolio_params) 
-          format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' } 
-        else 
-          format.html { render :edit } 
-        end 
-      end 
-    end 
-  
-    def show 
-    end 
+    render nothing: true 
+  end 
 
-    def destroy 
-      # Destroy/delete the record 
-      @portfolio_item.destroy 
 
-      # Redirect 
-      respond_to do |format| 
-        format.html { redirect_to portfolios_url, notice: 'Record was removed.' } 
-      end 
-    end 
+  def angular 
+    @angular_portfolio_items = Portfolio.angular 
+  end 
 
-    private 
-  
+  def new 
+    @portfolio_item = Portfolio.new 
+    3.times { @portfolio_item.technologies.build } 
+  end 
+
+  def create 
+    @portfolio_item = Portfolio.new(portfolio_params) 
  
-    def portfolio_params 
-      params.require(:portfolio).permit(:title, 
-                                        :subtitle, 
-                                        :body, 
-                                        technologies_attributes: [:name] 
-                                       ) 
+    respond_to do |format| 
+      if @portfolio_item.save 
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' } 
+      else 
+        format.html { render :new } 
+      end 
     end 
+  end 
 
-    def set_portfolio_item 
-      @portfolio_item = Portfolio.find(params[:id]) 
+  def edit 
+  end 
+  
+  def update 
+    respond_to do |format| 
+      if @portfolio_item.update(portfolio_params) 
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' } 
+      else 
+        format.html { render :edit } 
+      end 
     end 
+  end 
+  
+  def show 
+  end 
+
+  def destroy 
+    # Destroy/delete the record 
+    @portfolio_item.destroy 
+
+    # Redirect 
+    respond_to do |format| 
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' } 
+    end 
+  end 
+
+  private 
+  
+  def portfolio_params 
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name] 
+                                     ) 
+  end 
+
+  def set_portfolio_item 
+    @portfolio_item = Portfolio.find(params[:id]) 
+  end 
 
 end
