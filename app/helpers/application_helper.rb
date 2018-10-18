40744b8d898d5baaa34513ebca09187249a3855f
Pierre Le Count <pmlc@iinet.net.au>
2018-10-18 21:24:26 +0000

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,6 +61,16 @@ module ApplicationHelper
     "active" if current_page? path 
   end 
 
+  def alerts 
+    alert = (flash[:alert] || flash[:error] || flash[:notice]) 
 
+    if alert 
+      alert_generator alert 
+    end 
+  end 
+
+  def alert_generator msg 
+    js add_gritter(msg, title: "Pierre Le Counts  Portfolio", sticky: false) 
+  end 
 
 end
