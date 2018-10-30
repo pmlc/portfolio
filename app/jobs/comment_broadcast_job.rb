9a8f3cde0338f0ffd458a54902304752b382d9fa
Pierre Le Count <pmlc@iinet.net.au>
2018-10-30 22:10:57 +0000

    Created comment broadcast

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -2,12 +2,19 @@ class CommentBroadcastJob < ApplicationJob
   queue_as :default 
 
   def perform(comment) 
-    ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment) 
+    ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
   end 
 
   private 
 
   def render_comment(comment) 
+    puts "            "
+    puts "            "
+    puts "comments ==== ==== " + comment.to_s
+    puts "            "
+    puts "  I am in App/Jobs/comments_boradcast_job          "
+    puts "            "
+    puts "            "
     CommentsController.render partial: 'comments/comment', locals: { comment: comment } 
   end 
 
