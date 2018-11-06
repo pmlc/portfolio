733c210e397d79e80fc8fbe167bd351b5365bc00
Pierre Le Count <pmlc@iinet.net.au>
2018-11-06 22:38:56 +0000

    Minor modifcation of seeds file for Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,14 +17,14 @@ end
 
 puts "10 blog posts created" 
   
-5.times do |skill| 
-    Skill.create!( 
-      title: "Rails #{skill}", 
-      percent_utilized: 15 
-    ) 
-end 
+#5.times do |skill| 
+#    Skill.create!( 
+#      title: "Rails #{skill}", 
+#      percent_utilized: 15 
+#    ) 
+#end 
   
-puts "5 skills created" 
+#puts "5 skills created" 
   
  
 8.times do |portfolio_item| 
